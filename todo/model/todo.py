584cbb0d701f800cf6d8178ff4ee70bbe86abe17
samuel garcia <samuelgarcia201586@gmail.com>
2025-03-10 13:13:12 -0500

    primer commit

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,16 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed: bool = True
+
+    def add_tag(self,tags: list[str]):
+        self.tags = tags
+
+    def __str__(self):
+        return f"{self.code_id}-{self.title}"
