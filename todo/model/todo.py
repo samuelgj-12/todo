abccdb34b4125ea395e8becbb9eca8609f68f48c
samuel garcia <samuelgarcia201586@gmail.com>
2025-03-10 13:41:21 -0500

    class todobook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -13,4 +13,18 @@ class Todo:
         self.tags = tags
 
     def __str__(self):
-        return f"{self.code_id}-{self.title}"
+        return f"{self.code_id} - {self.title}"
+
+class TodoBook:
+    def __init__(self):
+        self.todos = {}
+        self.next_id = 1
+
+    def add_todo(self, title: str, description: str   ) -> int:
+        code_id = len(self.todos)+1
+        new_todo = Todo(code_id, title, description)
+        self.todos[code_id] = new_todo
+        return code_id
+
+    def pending_todo(self):
+        return [todo for todo in self.todos.values() if not todo.completed]
